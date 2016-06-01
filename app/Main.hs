f172cafe4137eba2bd3851cbdb850ab58a23a5d8
Santiago Saavedra <s.saavedra@fdi.ucm.es>
2016-06-01 15:42:27 +0200

    Remove Data.Strip example

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,7 @@
 module Main where
 
-import Data.String.Strip
 import Why3.AsProcess
 
 main :: IO ()
 main = do
   putStrLn $ show $ discharge Z3 "theory X goal a: true end"
-
--- main = interact strip
