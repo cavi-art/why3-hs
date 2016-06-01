5a2536204afd124389dc8eb210b75aca1a66d9e0
Santiago Saavedra <s.saavedra@fdi.ucm.es>
2016-06-01 16:01:48 +0200

    Change proof for something more complicated :P

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import Why3.AsProcess
 
 main :: IO ()
 main = do
-  putStrLn $ show $ discharge Z3 "theory X goal a: true end"
+  putStrLn $ show $ discharge Z3 "theory X goal a: forall a:int. a = a end"
