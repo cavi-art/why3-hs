0c1e5b2a0b096ba5a690c3ed32c7f71ba6d2aaa2
Santiago Saavedra <s.saavedra@fdi.ucm.es>
2016-06-09 18:59:06 +0200

    Add more functions to detect whether Why3 can be used.

--- src/Why3/AsProcess.hs
+++ src/Why3/AsProcess.hs
@@ -3,18 +3,31 @@ module Why3.AsProcess (
   discharge,
   isValidProof,
   dischargeTheory,
+  usableWhy3,
+  why3Provers,
 
   Prover(..),
   SMTOutput(..),
   VersionedProver
                       ) where
 
+import Control.Monad
 import Data.Char (toLower)
+import Data.Maybe (catMaybes)
+import qualified Data.Text as T
+import System.Exit (ExitCode(..))
 import System.IO.Unsafe (unsafePerformIO)
-import System.Process (readProcess)
+import System.Process (readProcess, readProcessWithExitCode)
+
+
+-- | This constant should probably be removed. We are trying to find
+-- why3 in the PATH, and this is the internal constant to keep track
+-- of that.
+pathToWhy3 = "why3"
 
 
 data Prover = AltErgo | CVC4 | Eprover | Gappa | Zenon | Z3
+  deriving (Eq, Show)
 
 -- We are not yet implementing them all, but they are here.
 -- The only ones implemented are valid, invalid and unknown.
@@ -24,8 +37,11 @@ data SMTOutput = Valid | Invalid | Unknown | Failure | Timeout String
 type VersionedProver = (Prover, Maybe String)
 
 discharge :: ProverSpec a => a -> String -> String -> SMTOutput
-discharge prover theoryImports = why3Output . getWhy3Output prover . embedFormulaInTheory
-  where embedFormulaInTheory f = "theory T " ++ theoryImports ++ " goal g: " ++ f ++ "end"
+discharge prover theoryImports = why3Output . getWhy3Output prover . embedFormulaInTheory theoryImports
+
+
+embedFormulaInTheory theoryImports f = "theory T " ++ theoryImports ++ " goal g: " ++ f ++ " end"
+
 
 dischargeTheory :: ProverSpec a => a -> String -> SMTOutput
 dischargeTheory prover = why3Output . getWhy3Output prover
@@ -40,7 +56,6 @@ getWhy3Output :: ProverSpec a => a -> String -> String
 getWhy3Output prover text =
   unsafePerformIO $ readProcess pathToWhy3 why3Params text
   where proverStr = stringFromProver prover
-        pathToWhy3 = "why3"
         why3Params = ["prove",
                       "-F", "why",
                       "--timelimit", "1", -- one second fixed timelimit for now
@@ -57,9 +72,53 @@ why3Output str = if theWord == "valid" then Valid
         theWord = map toLower $ takeWhile (/= ' ') fromColon
 
 
+-- | Find out whether Why3 can be called from Haskell.
+usableWhy3 = unsafePerformIO $ do
+  (exit, _, _) <- readProcessWithExitCode "which" [pathToWhy3] ""
+  return $ case exit of
+    ExitSuccess -> True
+    _ -> False
+
+
+why3Provers :: [(Prover, String)]
+why3Provers = unsafePerformIO $ proversList
+  where proversString = readProcess pathToWhy3 ["--list-provers"] ""
+        stripLine = T.unpack . T.strip . T.pack
+        proversList = catMaybes <$> (fmap stringToProver) <$> tail <$> (fmap (fmap stripLine) $ lines <$> proversString)
+{-# NOINLINE why3Provers #-}
+
+
+stringToProver = unLift . liftM2 (,) stringToProverName stringToProverVersion
+ where unLift (Just a, Just b) = Just (a, b)
+       unLift (_, _) = Nothing
+
+stringToProverVersion = Just . unparenthize . version
+  where unparenthize = init . tail -- remove first and last letters (should be parens)
+        version = head . tail . words -- second element of words of the argument
+
+-- | This function returns an incomplete list. There are some provers
+-- which are not implemented here. This could be bypassed by just
+-- using the raw string.
+stringToProverName s =
+  let proverName = head $ words s
+  in case proverName of
+    "Alt-Ergo" -> Just AltErgo
+    "CVC4" -> Just CVC4
+    "Eprover" -> Just Eprover
+    "Gappa" -> Just Gappa
+    "Z3" -> Just Z3
+    "Zenon" -> Just Zenon
+    _ -> Nothing
+
+
 class ProverSpec a where
   stringFromProver :: ProverSpec a => a -> String
 
+instance ProverSpec String where
+  stringFromProver = id
+
+instance ProverSpec (String, String) where
+  stringFromProver (prover, version) = prover ++ "," ++ version
 
 instance ProverSpec Prover where
   stringFromProver AltErgo = "alt-ergo"
--- test/Why3/AsProcessSpec.hs
+++ test/Why3/AsProcessSpec.hs
@@ -12,6 +12,11 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "To run this spec file" $ do
+    it "You need Why3 and Z3 in your system" $ do
+      usableWhy3 `shouldBe` True
+      length why3Provers `shouldSatisfy` (> 0)
+    
   describe "isValidProof" $ do
     it "is valid on sat formulas" $ do
       isValidProof Valid `shouldBe` True
@@ -19,11 +24,18 @@ spec = do
       \f -> (if f == Valid then True `shouldBe` True
              else isValidProof f `shouldBe` False)
 
+  describe "dischargeTheory" $ do
+    it "can discharge a valid proof (NEEDS Z3 and Why3 on your system)" $ do
+      dischargeTheory Z3 "theory T goal g: true end" `shouldBe` Valid
+    it "cannot discharge an invalid proof" $ do
+      dischargeTheory Z3 "theory T goal g: false end" `shouldNotBe` Valid
+
   describe "discharge" $ do
     it "can discharge a valid proof (NEEDS Z3 and Why3 on your system)" $ do
-      discharge Z3 "theory T goal g: true end" `shouldBe` Valid
-    it "cannot discharge an invalid proof" $ do
-      discharge Z3 "theory T goal g: false end" `shouldNotBe` Valid
+      discharge Z3 "" "true" `shouldBe` Valid
+    it "cannot discharge an obviously invalid proof" $ do
+      discharge Z3 "" "false" `shouldNotBe` Valid
+
 
 instance Arbitrary SMTOutput where
   arbitrary = oneof [return Valid,
